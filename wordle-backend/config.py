4acc7d515ff004abd39a21012aa3effa9e1edf4e
fidel-otieno2 <fidelotieno05@gmail.com>
2025-10-06 13:28:17 +0300

    Change to postgresql:// and psycopg2-binary for compatibility

--- wordle-backend/config.py
+++ wordle-backend/config.py
@@ -7,7 +7,9 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecret')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'supersecret')
     database_url = os.getenv('DATABASE_URL', 'postgresql://postgres:password@localhost/wordle')
-    if database_url and 'sslmode' not in database_url:
-        database_url += '?sslmode=require'
+    if database_url:
+        database_url = database_url.replace('postgresql+psycopg://', 'postgresql://')
+        if 'sslmode' not in database_url:
+            database_url += '?sslmode=require'
     SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
